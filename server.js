830398bdeb91bab5a3dc711cc780b2ebac19369c
Jess Luu <jessica.luu@edu.sait.ca>
2021-03-10 14:49:59 -0700

    added request log

--- server.js
+++ server.js
@@ -2,16 +2,50 @@ const { static } = require('express');
 const express = require('express');
 const app = express();
 const path = require("path");
+const fs = require("fs");
+
+let dataLog = function datalog(request, response, next) {
+  let dateTime = new Date();
+  let formattedDate =
+  dateTime.getFullYear() +
+  "-" +
+  (dateTime.getMonth() + 1) +
+  "-" +
+  dateTime.getDate() +
+  " " +
+  dateTime.getHours() +
+  ":" +
+  dateTime.getMinutes() +
+  ":" +
+  dateTime.getSeconds();
+  let method = request.method;
+  let url = request.url;
+  // let status = response.status();
+  let log = `${formattedDate} ${method}:${url}`;
+  console.log(log);
+  fs.appendFile("request_logs.txt", log + "\n", err => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  next(); 
+};
+
+app.use(dataLog);
 
 // simple approach; not the most robust
-app.use(express.static("./public"));
+// app.use(express.static("./public"));
 
 // preferred approach
-// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function(request, response){
   // console.log(request);
   response.send(`Hello world!`)
+});
+
+app.get("/about", (request, response) => {
+  response.send("This is the About page");
 });
 
 app.use(function(request, response){
@@ -21,7 +55,7 @@ app.use(function(request, response){
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, function(){
+app.listen(PORT, function() {
   console.log(`listening on port: ${PORT}`);
 })
 
